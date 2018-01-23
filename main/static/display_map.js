81f1aa51f85d4c2e991945416aed986dd86555b4
JamesSmith1202 <jsmith@stuy.edu>
2018-01-23 14:57:40 -0500

    got interactive map completely working

--- main/static/display_map.js
+++ main/static/display_map.js
@@ -1,26 +1,24 @@
 var canvas = document.getElementById('myCanvas');
-var floorImage = new Image()
-floorImage.src="http://127.0.0.1:5000/static/"+floor+".jpg";
-var mouseX;
-var mouseY;
+var floorImage;
 var radius = 10;
 var context = canvas.getContext('2d');
-canvas.width = floorImage.width;
-canvas.height = floorImage.height;
 
-floorImage.onload = function(){
-    context.drawImage(floorImage,0,0);
+var preloadImages = function(){
+    floorImage = new Image();
+    floorImage.src="http://127.0.0.1:5000/static/"+floor+".jpg";
+    floorImage.onload = function(){
+        canvas.width = floorImage.width;
+        canvas.height = floorImage.height;
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        context.drawImage(floorImage,0,0);
+        context.beginPath();
+        context.arc(coords[0], coords[1], radius, 0, 2 * Math.PI, false);
+        context.fillStyle = 'red';
+        context.fill();
+        context.lineWidth = 3;
+        context.strokeStyle = 'black';
+        context.stroke();
+    }
 }
 
-var placeDot = function(e){
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    context.drawImage(floorImage,0,0);
-    var bounds = canvas.getBoundingClientRect();
-    context.beginPath();
-    context.arc(coords[0], coords[1], radius, 0, 2 * Math.PI, false);
-    context.fillStyle = 'red';
-    context.fill();
-    context.lineWidth = 5;
-    context.strokeStyle = '#003300';
-    context.stroke();
-}
+window.onload = preloadImages;
